dd204ec31e6fc04d84f6dd498ea474da40f3e53b
hatake <hermit183@outlook.com>
2025-05-19 10:29:47 +0900

    printfilebar: use xsize

--- src/filebar.c
+++ src/filebar.c
@@ -1,13 +1,38 @@
 #include "filebar.h"
 
-#include "stdio.h"
+#include <stdio.h>
+#include <string.h>
 #include "portable.h"
+#include "system.h"
 
-char filepath[255];
-
-void printfilebar(void){
+void printfilebar(fileinfo *file){
 	invertcolor();
-	printf("~teko~    %s", filepath);
+	if(xsize >= strlen("teko"))
+	{
+		printf("teko");
+		if(xsize >= strlen(file->path) + strlen("teko"))
+		{
+			for(int i = 0; i < xsize - strlen(file->path) - strlen("teko"); i ++)
+			{
+				printf(" ");
+			}
+			printf("%s", file->path);
+		}
+		else
+		{
+			for(int i = 0; i < xsize - strlen("teko"); i ++)
+			{
+				printf(" ");
+			}
+		}
+	}
+	else
+	{
+		for(int i = 0; i < xsize; i ++)
+		{
+			printf(" ");
+		}
+	}
 	invertcolor();
 	printf("\n");
 }
